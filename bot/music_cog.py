6256530750d1169cfd1b8b7079827fee237a6d7c
jon-mey <jon.ae.meyer@gmail.com>
2024-08-18 21:10:25 +0200

    Skip cookie and hope for the best

--- bot/music_cog.py
+++ bot/music_cog.py
@@ -32,7 +32,7 @@ class music_cog(commands.Cog):
                 'source_address': '0.0.0.0', 
                 'http_headers': {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582',
-                    'Cookie': file.read()
+                #    'Cookie': file.read()
                 }
             }
 
